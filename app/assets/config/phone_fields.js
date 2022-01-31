2b806eb5652c178762576ec1c33e7d4f2e3abd75
André Moreira <andre.moreira@codeminer42.com>
2022-01-31 11:27:23 -0300

    adding default_phone fixes

--- app/assets/config/phone_fields.js
+++ app/assets/config/phone_fields.js
@@ -1,12 +1,15 @@
 function remove_fields(link) {
-  console.log(link)
-  console.log($(link).closest(".fields"))
   $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
+  $(link).closest(".phone_fields").hide();
 }
 
 function add_fields(link, association, content){
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g");
   $(link).parent().after(content.replace(regexp, new_id));
+}
+
+function uncheck_defaults(id){
+  $(".default-checkbox").prop("checked", false);
+  $("#"+id).prop("checked", true);
 }
