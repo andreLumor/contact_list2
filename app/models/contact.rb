db921b0cff28d6cf07bbe4955ce40c4587501fbb
André Moreira <andre.moreira@codeminer42.com>
2022-02-01 10:36:01 -0300

    adding hotwire to contacts page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,8 @@ class Contact < ApplicationRecord
   validates_uniqueness_of :name, scope: :user_id
   has_many :phones, dependent: :destroy
   accepts_nested_attributes_for :phones, reject_if: lambda { |a| a[:number].blank? }, allow_destroy: true
+
+  after_create_commit { broadcast_append_to "contacts"}
+  after_destroy_commit { broadcast_remove_to "contacts"}
+  after_update_commit { broadcast_update_to "contacts"}
 end
