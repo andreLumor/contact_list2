20f20e1ebac643ae2ba76fed11b9f395ac48293d
André Moreira <andre.moreira@codeminer42.com>
2022-02-04 13:45:32 -0300

    temporarily removing scopes to fix heroku

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
-  validates_uniqueness_of :name, scope: :user_id
+  #validates_uniqueness_of :name, scope: :user_id
   has_many :phones, dependent: :destroy
   accepts_nested_attributes_for :phones, reject_if: lambda { |a| a[:number].blank? }, allow_destroy: true
 
