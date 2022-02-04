951934698f335470f5d8a4c3f98ea0f781ab8aa8
André Moreira <andre.moreira@codeminer42.com>
2022-02-04 13:57:11 -0300

    put files back to fix heroku

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,7 +1,7 @@
 class Phone < ApplicationRecord
   belongs_to :contact
   validates :number, presence: true
-  #validates_uniqueness_of :number, scope: :contact_id
+  validates_uniqueness_of :number, scope: :contact_id
   validates_inclusion_of :default_phone, :in => [true, false]
 
   
